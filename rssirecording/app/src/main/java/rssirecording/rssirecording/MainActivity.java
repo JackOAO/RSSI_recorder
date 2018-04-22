2dc21eea7d85abc1d19d07636d1682fff2cf5b50
Unknown <wert943@gmail.com>
2018-04-22 23:18:33 +0800

    asdsadasd

--- rssirecording/app/src/main/java/rssirecording/rssirecording/MainActivity.java
+++ rssirecording/app/src/main/java/rssirecording/rssirecording/MainActivity.java
@@ -6,6 +6,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
